f04e6786896dd66b19dcb193bec022fa37708583
mimhle <40392166+mimhle@users.noreply.github.com>
2024-07-01 15:49:42 +0700

    Test

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return os.environ.get("db_host", "No db_host found in environment variables")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
