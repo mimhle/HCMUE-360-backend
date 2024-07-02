d9f1054c48186cc7192591ce029b3a8f2d9071df
mimhle <40392166+mimhle@users.noreply.github.com>
2024-07-02 15:41:57 +0700

    Added endpoints

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from database.database import get_scene
-from flask import Flask
+from database.database import get_scene as get_scene_, get_scenes as get_scenes_, update_scene as update_scene_
+from flask import Flask, request, Response
 import os
 
 
@@ -18,9 +18,22 @@ def hello_world():
     return DB["db"]
 
 
+@app.route("/scenes", methods=["GET"])
+def get_scenes() -> list[dict]:
+    return get_scenes_(db=DB)
+
+
 @app.route("/scenes/<int:id_>", methods=["GET"])
-def get_scene_(id_: int) -> dict:
-    return get_scene(int(id_), db=DB)
+def get_scene(id_: int) -> dict:
+    return get_scene_(int(id_), db=DB)
+
+
+@app.route("/scenes/<int:id_>", methods=["PUT"])
+def update_scene(id_: int) -> dict | Response:
+    data = request.get_json()
+    if not data:
+        return Response(status=400)
+    return update_scene_(id_, data, db=DB)
 
 
 if __name__ == "__main__":
--- database/database.py
+++ database/database.py
@@ -1,5 +1,4 @@
 import json
-
 import pymongo
 
 
@@ -10,3 +9,22 @@ def get_scene(id_: int, *, db: dict = None) -> dict:
     with pymongo.MongoClient(db["url"]) as client:
         collection = client[db["db"]]["Scenes"]
         return json.loads(json.dumps(collection.find_one({"id": id_}, {"_id": 0}), default=str))
+
+
+def get_scenes(*, db: dict | None = None) -> list[dict]:
+    if not db:
+        raise ValueError("db must be provided")
+
+    with pymongo.MongoClient(db["url"]) as client:
+        collection = client[db["db"]]["Scenes"]
+        result = [item for item in collection.find({}, {"id": 1, "name": 1, "_id": 0}, sort=[("_id", 1)])]
+        return result
+
+
+def update_scene(id_: int, data: dict, *, db: dict | None = None) -> dict:
+    if not db:
+        raise ValueError("db must be provided")
+
+    with pymongo.MongoClient(db["url"]) as client:
+        collection = client[db["db"]]["Scenes"]
+        return collection.find_one_and_replace({"id": id_}, data)
